5051d7e671b489846609475eb6f6facb6d94b66d
Larissa S <larissafschiavo@gmail.com>
2017-09-27 16:44:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board
+print "   " + "|" + "   " + "|" + "   "
+print "\n-----------\n"
+print  "   " + "|" + "   " + "|" + "   "
+print "\n-----------\n"
+print  "   " + "|" + "   " + "|" + "   " + "\n"
+end
+
+def display_board(board)
+  if board.empty?
+    print display_board
+end
