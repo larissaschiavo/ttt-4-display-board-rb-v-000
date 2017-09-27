81a39c7f1cfcad97f325704f86974b60755db310
Larissa S <larissafschiavo@gmail.com>
2017-09-27 16:48:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 
 def display_board(board)
-print " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
-print "\n-----------\n"
-print  " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
-print "\n-----------\n"
-print  " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} " + "\n"
+  print " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
+  print "\n-----------\n"
+  print  " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+  print "\n-----------\n"
+  print  " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} " + "\n"
 end
